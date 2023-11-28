b9eaad5251f8a0b4d1bdbcf03d75730643be3e73
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-11-28 15:06:03 +0300

    y

--- required.py
+++ required.py
@@ -106,3 +106,33 @@ def to_padded_chars(integer, max_len=3, pad_right=False):
         return str(integer) + padding
     else:
         return padding + str(integer)
+
+# Illustrate the generated query/answer pairs
+
+unique_characters = '0123456789+- '       # All unique characters that are used in the queries (13 in total: digits 0-9, 2 operands [+, -], and a space character ' '.)
+highest_integer = 99                      # Highest value of integers contained in the queries
+
+max_int_length = len(str(highest_integer))# Maximum number of characters in an integer
+max_query_length = max_int_length * 2 + 1 # Maximum length of the query string (consists of two integers and an operand [e.g. '22+10'])
+max_answer_length = 3    # Maximum length of the answer string (the longest resulting query string is ' 1-99'='-98')
+
+# Create the data (might take around a minute)
+(MNIST_data, MNIST_labels), _ = tf.keras.datasets.mnist.load_data()
+X_text, X_img, y_text, y_img = create_data(highest_integer)
+print(X_text.shape, X_img.shape, y_text.shape, y_img.shape)
+
+
+## Display the samples that were created
+def display_sample(n):
+    labels = ['X_img:', 'y_img:']
+    for i, data in enumerate([X_img, y_img]):
+        plt.subplot(1,2,i+1)
+        # plt.set_figheight(15)
+        plt.axis('off')
+        plt.title(labels[i])
+        plt.imshow(np.hstack(data[n]), cmap='gray')
+    print('='*50, f'\nQuery #{n}\n\nX_text: "{X_text[n]}" = y_text: "{y_text[n]}"')
+    plt.show()
+
+for _ in range(10):
+    display_sample(np.random.randint(0, 10000, 1)[0])
