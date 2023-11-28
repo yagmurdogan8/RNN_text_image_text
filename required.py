f8c47ae1778a021a437eb13cfdc2c3bdcc6c60c1
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-11-28 15:05:05 +0300

    y

--- required.py
+++ required.py
@@ -11,33 +11,37 @@ from scipy.ndimage import rotate
 
 # Create plus/minus operand signs
 def generate_images(number_of_images=50, sign='-'):
-
     blank_images = np.zeros([number_of_images, 28, 28])  # Dimensionality matches the size of MNIST images (28x28)
-    x = np.random.randint(12, 16, (number_of_images, 2)) # Randomized x coordinates
-    y1 = np.random.randint(6, 10, number_of_images)       # Randomized y coordinates
-    y2 = np.random.randint(18, 22, number_of_images)     # -||-
+    x = np.random.randint(12, 16, (number_of_images, 2))  # Randomized x coordinates
+    y1 = np.random.randint(6, 10, number_of_images)  # Randomized y coordinates
+    y2 = np.random.randint(18, 22, number_of_images)  # -||-
 
-    for i in range(number_of_images): # Generate n different images
-        cv2.line(blank_images[i], (y1[i], x[i,0]), (y2[i], x[i, 1]), (255,0,0), 2, cv2.LINE_AA)     # Draw lines with randomized coordinates
+    for i in range(number_of_images):  # Generate n different images
+        cv2.line(blank_images[i], (y1[i], x[i, 0]), (y2[i], x[i, 1]), (255, 0, 0), 2, cv2.LINE_AA)  # Draw lines with
+        # randomized coordinates
         if sign == '+':
-            cv2.line(blank_images[i], (x[i,0], y1[i]), (x[i, 1], y2[i]), (255,0,0), 2, cv2.LINE_AA) # Draw lines with randomized coordinates
+            cv2.line(blank_images[i], (x[i, 0], y1[i]), (x[i, 1], y2[i]), (255, 0, 0), 2,
+                     cv2.LINE_AA)  # Draw lines with
+            # randomized coordinates
         if sign == '*':
-            cv2.line(blank_images[i], (x[i,0], y1[i]), (x[i, 1], y2[i]), (255,0,0), 2, cv2.LINE_AA)
+            cv2.line(blank_images[i], (x[i, 0], y1[i]), (x[i, 1], y2[i]), (255, 0, 0), 2, cv2.LINE_AA)
             # Rotate 45 degrees
             blank_images[i] = rotate(blank_images[i], -50, reshape=False)
-            cv2.line(blank_images[i], (x[i,0], y1[i]), (x[i, 1], y2[i]), (255,0,0), 2, cv2.LINE_AA)
+            cv2.line(blank_images[i], (x[i, 0], y1[i]), (x[i, 1], y2[i]), (255, 0, 0), 2, cv2.LINE_AA)
             blank_images[i] = rotate(blank_images[i], -50, reshape=False)
-            cv2.line(blank_images[i], (x[i,0], y1[i]), (x[i, 1], y2[i]), (255,0,0), 2, cv2.LINE_AA)
+            cv2.line(blank_images[i], (x[i, 0], y1[i]), (x[i, 1], y2[i]), (255, 0, 0), 2, cv2.LINE_AA)
 
     return blank_images
 
+
 def show_generated(images, n=5):
     plt.figure(figsize=(2, 2))
-    for i in range(n**2):
-        plt.subplot(n, n, i+1)
+    for i in range(n ** 2):
+        plt.subplot(n, n, i + 1)
         plt.axis('off')
         plt.imshow(images[i])
     plt.show()
+
 
 show_generated(generate_images())
 show_generated(generate_images(sign='+'))
