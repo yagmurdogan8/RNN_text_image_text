5e3d7e67270f7e98efdb1b478a8cfd64fd77dcdf
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-12-03 16:30:10 +0300

    y

--- text_to_image.py
+++ text_to_image.py
@@ -1,6 +1,10 @@
+import numpy as np
 import tensorflow as tf
 from keras.layers import Dense, LSTM, TimeDistributed
 from keras.layers import RepeatVector, Conv2D, Reshape, Conv2DTranspose
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
 import required
 
@@ -26,3 +30,39 @@ def build_text2image_model(use_deconv=True, filters=512):
     text2image.summary()
 
     return text2image
+
+
+X_train_onehot, X_test_onehot, y_train_onehot, y_test_onehot = train_test_split(required.X_text_onehot,
+                                                                                required.y_text_onehot,
+                                                                                test_size=0.1, random_state=42)
+
+text2text = build_text2image_model()
+
+# Train the model
+text2text.fit(X_train_onehot, y_train_onehot, epochs=50, batch_size=128, validation_split=0.1)
+
+# Evaluate the model
+score = text2text.evaluate(X_test_onehot, y_test_onehot, batch_size=128)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Confusion matrix
+y_pred = text2text.predict(X_test_onehot)
+y_pred = np.argmax(y_pred, axis=2)
+y_test = np.argmax(y_test_onehot, axis=2)
+
+# Flatten the arrays
+y_pred = y_pred.flatten()
+y_test = y_test.flatten()
+
+cm = confusion_matrix(y_test, y_pred) / len(y_test)
+
+# Plot confusion matrix
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, cmap='Blues')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.colorbar(format='%d%%')
+# Add labels in each cell
+
+plt.show()
