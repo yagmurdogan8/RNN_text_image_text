9762779f1fb4df1770ce0950d8977a03736697fc
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-12-03 16:21:56 +0300

    y

--- text_to_image.py
+++ text_to_image.py
@@ -7,20 +7,15 @@ import required
 
 def build_text2image_model(use_deconv=True, filters=512):
     text2image = tf.keras.Sequential()
-    # text2image.add(Embedding(len(unique_characters), 128, input_length=5))
     text2image.add(LSTM(256, input_shape=(None, len(required.unique_characters))))
-    # text2image.add(Dense(256))
     text2image.add(RepeatVector(required.max_answer_length))
-    # text2image.add(LSTM(256, return_sequences=True))
-    # text2image.add(TimeDistributed(Dense(len(unique_characters), activation="softmax")))
 
-    if (use_deconv):
+    if use_deconv:
         text2image.add(TimeDistributed(Dense(7 * 7 * 128, activation="softmax")))
         text2image.add(TimeDistributed(Reshape((7, 7, 128))))
         text2image.add(TimeDistributed(required.BatchNormalization()))
         text2image.add(
             TimeDistributed(Conv2DTranspose(filters, (3, 3), strides=(2, 2), padding="same", activation="relu")))
-        # text2image.add(TimeDistributed(Conv2DTranspose(1, (3, 3), strides=(7, 7), padding="same", activation="relu")))
         text2image.add(TimeDistributed(required.BatchNormalization()))
         text2image.add(
             TimeDistributed(Conv2DTranspose(filters, (3, 3), strides=(2, 2), padding="same", activation="relu")))
