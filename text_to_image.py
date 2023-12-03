fad6883c4cef20176570a0dfddad2afd51675f76
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-12-03 16:42:26 +0300

    y

--- text_to_image.py
+++ text_to_image.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from keras.layers import Dense, LSTM, TimeDistributed
 from keras.layers import RepeatVector, Conv2D, Reshape, Conv2DTranspose
+from keras.src.layers import BatchNormalization
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
@@ -17,13 +18,13 @@ def build_text2image_model(use_deconv=True, filters=512):
     if use_deconv:
         text2image.add(TimeDistributed(Dense(7 * 7 * 128, activation="softmax")))
         text2image.add(TimeDistributed(Reshape((7, 7, 128))))
-        text2image.add(TimeDistributed(required.BatchNormalization()))
+        text2image.add(TimeDistributed(BatchNormalization()))
         text2image.add(
             TimeDistributed(Conv2DTranspose(filters, (3, 3), strides=(2, 2), padding="same", activation="relu")))
-        text2image.add(TimeDistributed(required.BatchNormalization()))
+        text2image.add(TimeDistributed(BatchNormalization()))
         text2image.add(
             TimeDistributed(Conv2DTranspose(filters, (3, 3), strides=(2, 2), padding="same", activation="relu")))
-        text2image.add(TimeDistributed(required.BatchNormalization()))
+        text2image.add(TimeDistributed(BatchNormalization()))
         text2image.add(TimeDistributed(Conv2D(1, (5, 5), padding="same", activation="sigmoid")))
 
     text2image.compile(loss='binary_crossentropy', optimizer='adam')  # mse loss increase the accuracy
